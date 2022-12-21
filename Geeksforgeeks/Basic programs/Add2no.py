3df515e14c992ab28d8ab0ca4984c9b506766169
Vinutha-B-M <114577005+Vinutha-B-M@users.noreply.github.com>
2022-12-21 19:26:24 +0530

    adding 2 nos

---
+++ Geeksforgeeks/Basic programs/Add2no.py
@@ -0,0 +1,17 @@
+#write a Python program to find the addition of these two numbers
+
+a = int(input("Enter one number : "))
+b = int(input("Enter second number : "))
+sum = a + b
+print(sum)
+
+#===========================================
+#second way
+
+def add(x,y):
+    return x
+
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+print("addition of two numbers is : " ,add(a,b))    
