1644d87cca0f54c9bd242b4167f1119dd2e2e42d
Vinutha-B-M <114577005+Vinutha-B-M@users.noreply.github.com>
2022-12-26 14:58:24 +0530

    modified

--- Geeksforgeeks/Basic programs/palindrome.py
+++ Geeksforgeeks/Basic programs/palindrome.py
@@ -17,3 +17,13 @@ if a == Palindrome(a):
     print("is palindrome")
 else:
     print("is not palindrome")
+
+#====================================
+a = "hello"
+b = ""
+for i in a:
+    b = i + b
+if (a == b):
+    print("Yes")
+else:
+    print("No")
