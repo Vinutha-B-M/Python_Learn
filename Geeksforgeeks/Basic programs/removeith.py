ff624c183a6a5cef199aa39f3b14837450b0f061
Vinutha-B-M <114577005+Vinutha-B-M@users.noreply.github.com>
2022-12-26 16:53:44 +0530

    remove ith char frm str

---
+++ Geeksforgeeks/Basic programs/removeith.py
@@ -0,0 +1,18 @@
+#Ways to remove i’th character from string in Python
+
+a = "welcome"
+b = ''
+pos = 5
+for i in range(len(a)):
+    if i != pos:
+        b = b + a[i]
+print(b)
+
+#===================================
+a = input("Enter a word : ")
+b = ''
+pos = int(input("Enter position : "))
+for i in range(len(a)):
+    if i != pos:
+        b = b + a[i]
+print(b)
