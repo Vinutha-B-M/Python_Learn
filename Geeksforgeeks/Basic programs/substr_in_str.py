20908a881ccc62d3aa5344950a0b59c021bbac38
Vinutha-B-M <114577005+Vinutha-B-M@users.noreply.github.com>
2022-12-26 22:24:34 +0530

    check substring in given str

---
+++ Geeksforgeeks/Basic programs/substr_in_str.py
@@ -0,0 +1,18 @@
+#Python | Check if a Substring is Present in a Given String
+
+a = "I am vinutha"
+print("yes" if 'am' in a else "no")
+
+#================================
+a = input("Enter a string : ")
+b = input("Enter a substring : ")
+print("yes" if b in a else "no")
+
+#==================================
+a = input("Enter a string : ")
+b = input("Enter a substring : ")
+if a.find(b) == -1:
+    print("is not substring")
+else:
+    print("is substring")
+
