6fa895436c8c6e527b4062c3bbd592c9c0a5c6ea
Vinutha-B-M <114577005+Vinutha-B-M@users.noreply.github.com>
2022-12-28 08:35:21 +0530

    sum of all items in a dictionary

--- Geeksforgeeks/Basic programs/sumofvalindict.py
+++ Geeksforgeeks/Basic programs/sumofvalindict.py
@@ -16,3 +16,10 @@ list = []
 for i in myDict: 
     list.append(myDict[i]) 
 print("sum is ",sum(list))
+
+#=========================
+myDict = {'a': 100, 'b': 200, 'c': 300} 
+sum = 0
+for i in myDict.values(): 
+    sum = sum + i
+print(sum)
