dace4d2476c8605ea016f23c7b0201a07923997c
Vinutha-B-M <114577005+Vinutha-B-M@users.noreply.github.com>
2023-02-02 08:48:18 +0530

    Create programiztables.py

---
+++ Geeksforgeeks/Basic programs/programiztables.py
@@ -0,0 +1,21 @@
+num = int(input("Enter a number : "))
+
+count = 10
+while count <= 10:
+    if count >= 1:
+        product = num * count
+        print(num, "x" , count, "=" , product)
+        count = count - 1
+        
+o/p:
+  Enter a number : 6
+6 x 10 = 60
+6 x 9 = 54
+6 x 8 = 48
+6 x 7 = 42
+6 x 6 = 36
+6 x 5 = 30
+6 x 4 = 24
+6 x 3 = 18
+6 x 2 = 12
+6 x 1 = 6
